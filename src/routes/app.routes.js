84ecda4a774594911c8ce090283ce959f0be7909
ronaldoapf <ronaldo.alves.1997@gmail.com>
2020-08-19 14:51:56 -0300

    new: create Typography component

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
+import SandboxRonaldo from '../pages/SandboxRonaldo';
 import Dashboard from '../pages/Dashboard';
 import CustomDrawer from './CustomDrawer';
 
@@ -8,7 +9,8 @@ const AppStack = createDrawerNavigator();
 
 const AppRoutes = () => (
   <AppStack.Navigator drawerType="slide" drawerContent={(props) => <CustomDrawer {...props} />}>
-    <AppStack.Screen name="Dashboard" component={Dashboard} />
+    {/* <AppStack.Screen name="Dashboard" component={Dashboard} /> */}
+    <AppStack.Screen name="SandboxRonaldo" component={SandboxRonaldo} />
   </AppStack.Navigator>
 );
 
