27ff8f61d8590b32fc37157ebc7159a4513b6d94
franco <gabrielfdg10@gmail.com>
2020-08-19 22:41:09 -0300

    new: create Drawer component

--- src/routes/CustomDrawer.js
+++ src/routes/CustomDrawer.js
@@ -1,8 +1,49 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, TouchableOpacity } from 'react-native';
+
+import Typography from '../components/Typography';
+import Icon from '../components/Icon';
+
+import {
+  DrawerContainer,
+  UserInfo,
+  UserImage,
+  UserName,
+  UserEmail,
+  HeaderRow,
+  MenuItem,
+  MenuContainer,
+} from './style';
 
 const CustomDrawer = () => (
-  <View />
+  <DrawerContainer>
+    <HeaderRow>
+      <UserInfo>
+        <UserImage />
+        <View>
+          <UserName> John Doe </UserName>
+          <UserEmail> john.doe@example.com </UserEmail>
+        </View>
+      </UserInfo>
+      <Icon lib="MaterialDesign" iconName="sort" size={24} color="#666" />
+    </HeaderRow>
+    <MenuContainer>
+      <View>
+        <MenuItem>
+          <Typography variant="subtitle"> Início </Typography>
+        </MenuItem>
+        <MenuItem>
+          <Typography variant="subtitle"> Meus serviços </Typography>
+        </MenuItem>
+        <MenuItem>
+          <Typography variant="subtitle"> Oferecer um serviço </Typography>
+        </MenuItem>
+      </View>
+      <MenuItem>
+        <Typography variant="subtitle"> Sair </Typography>
+      </MenuItem>
+    </MenuContainer>
+  </DrawerContainer>
 );
 
 export default CustomDrawer;
