c6bd5d8768de2fad09eda809c722ced005753382
ronaldoapf <ronaldo.alves.1997@gmail.com>
2020-08-21 00:32:38 -0300

    update: remove comments

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import SandboxRonaldo from '../pages/SandboxRonaldo';
-// import SandboxJoao from '../pages/SandboxJoao';
-// import SandboxFranco from '../pages/SandboxFranco';
-// import SandboxEmmanuel from '../pages/SandboxEmmanuel';
+import SandboxJoao from '../pages/SandboxJoao';
+import SandboxFranco from '../pages/SandboxFranco';
+import SandboxEmmanuel from '../pages/SandboxEmmanuel';
 
-// import Dashboard from '../pages/Dashboard';
-// import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+import SignUp from '../pages/SignUp';
 import CustomDrawer from './CustomDrawer';
 
 const AppStack = createDrawerNavigator();
 
 const AppRoutes = () => (
   <AppStack.Navigator drawerType="slide" drawerContent={(props) => <CustomDrawer {...props} />}>
-    {/* <AppStack.Screen name="Dashboard" component={Dashboard} />
+    <AppStack.Screen name="Dashboard" component={Dashboard} />
     <AppStack.Screen name="SignUp" component={SignUp} />
     <AppStack.Screen name="SandboxJoao" component={SandboxJoao} />
-  <AppStack.Screen name="SandboxEmmanuel" component={SandboxEmmanuel} /> */}
-    {/* <AppStack.Screen name="SandboxFranco" component={SandboxFranco} /> */}
+    <AppStack.Screen name="SandboxEmmanuel" component={SandboxEmmanuel} />
+    <AppStack.Screen name="SandboxFranco" component={SandboxFranco} />
     <AppStack.Screen name="SandboxRonaldo" component={SandboxRonaldo} />
   </AppStack.Navigator>
 );
