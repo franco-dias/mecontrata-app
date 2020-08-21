ed2d274570334ffee8505d977fdff26373d87ce6
franco <gabrielfdg10@gmail.com>
2020-08-21 00:23:18 -0300

    fix: update topbar icon positions

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -14,12 +14,12 @@ const AppStack = createDrawerNavigator();
 
 const AppRoutes = () => (
   <AppStack.Navigator drawerType="slide" drawerContent={(props) => <CustomDrawer {...props} />}>
-    <AppStack.Screen name="Dashboard" component={Dashboard} />
-    <AppStack.Screen name="SandboxRonaldo" component={SandboxRonaldo} />
-    <AppStack.Screen name="SignUp" component={SignUp} />
-    <AppStack.Screen name="SandboxJoao" component={SandboxJoao} />
+    {/* <AppStack.Screen name="Dashboard" component={Dashboard} /> */}
+    {/* <AppStack.Screen name="SandboxRonaldo" component={SandboxRonaldo} /> */}
+    {/* <AppStack.Screen name="SignUp" component={SignUp} /> */}
+    {/* <AppStack.Screen name="SandboxJoao" component={SandboxJoao} /> */}
     <AppStack.Screen name="SandboxFranco" component={SandboxFranco} />
-    <AppStack.Screen name="SandboxEmmanuel" component={SandboxEmmanuel} />
+    {/* <AppStack.Screen name="SandboxEmmanuel" component={SandboxEmmanuel} /> */}
   </AppStack.Navigator>
 );
 
