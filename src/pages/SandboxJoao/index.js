58e7c7d97e60580bfa53b728d0684a6d4ca094ca
JoaoVitorPereiraUFU <joaovitorpessoal2@gmail.com>
2020-08-22 10:21:45 -0300

    new: Adicao de componente de galeria

--- src/pages/SandboxJoao/index.js
+++ src/pages/SandboxJoao/index.js
@@ -4,11 +4,13 @@ import {
 } from 'react-native';
 import CategoryCard from '../../components/CategoryCard';
 import TextArea from '../../components/TextArea';
+import Gallery from '../../components/Gallery';
 
 const DashboardPage = () => (
   <View style={{ flex: 1, alignItems: 'center', padding: 24 }}>
     <CategoryCard title="Administração, negócios e serviços" color="#cc0066" />
     <TextArea placeHolder="Descrição" />
+    <Gallery photos={['https://avatars3.githubusercontent.com/u/33707685?s=60&v=4', 'https://avatars3.githubusercontent.com/u/33707685?s=60&v=4']} />
   </View>
 );
 
